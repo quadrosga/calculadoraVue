b918a960113221fa802238002d21c2ad60adbaec
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2024-06-11 18:04:54 -0300

    criando calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,77 @@
 <script setup>
+import { reactive } from 'vue';
+
+const estado = reactive({
+    num1: 0,
+    num2: 0,
+    operacao: 'soma',
+    resultado: null,
+})
+
+function calcularResultado() {
+    switch (estado.operacao) {
+        case 'soma':
+            estado.resultado = soma();
+            break;
+        case 'subtracao':
+            estado.resultado = subtracao();
+            break;
+        case 'multiplicacao':
+            estado.resultado = multiplicacao();
+            break;
+        case 'divisao':
+            estado.resultado = divisão();
+            break;
+    }
+}
+
+function soma() {
+    const { num1, num2 } = estado;
+    return num1 + num2;
+}
+
+function subtracao() {
+    const { num1, num2 } = estado;
+    return num1 - num2;
+}
+
+function multiplicacao() {
+    const { num1, num2 } = estado;
+    return num1 * num2;
+}
+
+function divisão() {
+    const { num1, num2 } = estado;
+    if (num2 === 0) {
+        alert("Denominador não pode ser 0")
+        return null;
+    }
+    else {
+        return num1 / num2;
+    }
+}
 
 </script>
 
 <template>
+<h1>Calculadora aritmética</h1>
+<br />
+<label>Primeiro número:</label>
+<input type="number" v-model="estado.num1" placeholder="Insira um número" class="form-control">
+<br />
+<label>Segundo número:</label>
+<input type="number" v-model="estado.num2" placeholder="Insira um número">
+<br />
+<label>Selecionar operação:</label>
+<select v-model="estado.operacao" @change="calcularResultado" class="form-select">
+    <option value="soma">Soma</option>
+    <option value="subtracao">Subtração</option>
+    <option value="multiplicacao">Multiplicação</option>
+    <option value="divisao">Divisão</option>
+</select>
+<br />
+<label>Resultado:</label>
+<input type="number" :value="estado.resultado" readonly>
 
 </template>
 
